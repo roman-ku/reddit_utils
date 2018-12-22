10739cc48c2fc59ee224b1a7459ec8e6ceeb5f31
roman-ku <roman-ku@users.noreply.github.com>
2018-12-22 03:02:36 -0800

    Fix build errors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='reddit_utils',
-    version='1.0.0',
+    version='1.0.4',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
